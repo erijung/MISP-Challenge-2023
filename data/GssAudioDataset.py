c47bcd1a331692e1a863be9eeaeaec0a1b91d171
erijung <erijung@berkeley.edu>
2023-12-21 09:44:29 +0800

    fix to point to only the enhanced files

--- data/GssAudioDataset.py
+++ data/GssAudioDataset.py
@@ -3,13 +3,14 @@ from torchaudio import load
 import torch
 import os
 import glob
+import random
 
 class GssSimulatedAudioDataset(Dataset):
     """
     Dataset for (X: Gss Audio Files, Y: Clean Audio Files)
     """
 
-    def __init__(self, gss_wav_dir, label_dir, transform=None):
+    def __init__(self, gss_wav_dir, label_dir, file_list, transform=None):
         """
         Args:
             data_dir (str): Path to the directory containing audio files.
@@ -20,10 +21,14 @@ class GssSimulatedAudioDataset(Dataset):
         self.label_dir = label_dir
         self.transform = transform
 
+        file_list = open(file_list, "r")
+        gss_audio_files = file_list.read().split('\n')
+        self.gss_audio_files = gss_audio_files
+        file_list.close()
         # List all audio files in the input directory
         # Note that his is nested so {session}_{noise_lvl}db-{source}-{timestamp1}_{timestamp2}.wav
         # has full path as input_dir/{session}_{noise_lvl}db/{session}_{noise_lvl}db-{source}-{timestamp1}_{timestamp2}.wav
-        self.gss_audio_files = [os.path.join(root.split('/')[-1], f) for root, sub, flist in os.walk(self.data_dir) for f in flist]
+        # self.gss_audio_files = [os.path.join(root.split('/')[-1], f) for root, sub, flist in os.walk(self.data_dir) for f in flist]
         # print(self.gss_audio_files)
         # self.gss_audio_files = self.gss_audio_files[:1000]
 
@@ -31,7 +36,7 @@ class GssSimulatedAudioDataset(Dataset):
         return len(self.gss_audio_files)
     
     def truncate_data(self, samples):
-        self.gss_audio_files.shuffle()
+        random.shuffle(self.gss_audio_files)
         self.gss_audio_files = self.gss_audio_files[:samples]
 
     def __getitem__(self, idx):
@@ -45,7 +50,7 @@ class GssSimulatedAudioDataset(Dataset):
         source, timestamps_wav = source_timestamps.split('-') # timestamps_wav = {timestamp1}_{timestamp2}.wav
 
         # Construct the input and label file path
-        audio_file_path = os.path.join(self.data_dir, input_file)
+        audio_file_path = os.path.join(self.data_dir, session_noise_lvl + 'db', input_file)
         label_file_path = os.path.join(self.label_dir, f"{source}-{session}-{timestamps_wav}")
         
         # Load the audio file using torchaudio
@@ -66,11 +71,12 @@ class GssSimulatedAudioDataset(Dataset):
             clean_waveform, clean_sample_rate = load(label_file_path)
         assert(sample_rate == clean_sample_rate)
         if clean_waveform.shape != input_waveform.shape:
-            max_length = max([input_waveform.shape[1], clean_waveform.shape[1]])
+            max_length = max([input_waveform.shape[1 ], clean_waveform.shape[1]])
             # print('padding input with', max_length - input_waveform.shape[1])
             input_waveform = pad_waveform(input_waveform, max_length)
             # print('padding target with', max_length - clean_waveform.shape[1])
             clean_waveform = pad_waveform(clean_waveform, max_length)
+            input_waveform = input_waveform + clean_waveform
 
         # Apply any specified transforms
         if self.transform:
